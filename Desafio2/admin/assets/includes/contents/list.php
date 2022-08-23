94d07fb38f002d212629d2fc566553c172730a20
Riki <lucas.ringelstein00@gmail.com>
2022-08-23 13:04:24 -0300

    1.3

--- Desafio2/admin/assets/includes/contents/list.php
+++ Desafio2/admin/assets/includes/contents/list.php
@@ -3,9 +3,41 @@
 ?>
 <!DOCTYPE html>
 <body>
-    <?php
-        $contents=new Clases\Contents();
-        $contents->list();
-    ?>
+    <div>
+        <div class="row">
+            <div class="col">ID</div>
+            <div class="col">TITULO</div>
+            <div class="col">CONTENIDO</div>
+            <div class="col">PALABRA CLAVE</div>
+            <div class="col">DESCRIPCION</div>
+            <div class="col">CATEGORIA</div>
+        </div>
+        <?php
+            // Se crean las variables necesarias
+            $content=new Clases\Contents();
+            $contents=array();
+            $contents["items"] = array();
+
+            // Se llama a la funcion
+            $res = $content->list();
+
+            // Se pregunta si existen filas en la tabla
+            if($res->rowCount()){
+                while ($row = $res->fetch(PDO::FETCH_ASSOC)){
+                        $id = $row['id'];
+                        $title = $row['title'];
+                        $content= $row['content'];
+                        $keywords = $row['keywords'];
+                        $description = $row['description'];
+                        $category = $row['category'];
+                        ?>
+                }
+
+            }else{
+                echo "No hay elementos en la tabla";
+            }
+
+        ?>
+    </div>
 </body>
 </html>
